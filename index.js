5a6ed5201a39c6ff9ba1b4e8f3040df71915bf03
vistritpandey <vistritpandey@gmail.com>
2020-10-05 18:33:05 +0530

    f

--- index.js
+++ index.js
@@ -12,10 +12,26 @@ document.addEventListener('DOMContentLoaded', () =>{
     doodler.style.left = doodlerLeftSpace + 'px'
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
+
+  class Platform{
+    constructor(newPlatBottom){
+      this.bottom = newPlatBottom
+      this.left = Math.random()*315
+      this.visual = document.createElement('div')
+
+      const visual =this.visual
+      visual.classList.add('platform')
+      visual.style.left = this.left +'px'
+      visual.style.bottom = this.bottom +'px'
+      grid.appendChild(visual)
+    }
+  }
   function createPlatforms(){
     for (let i=0;i<platformCount;i++){
       let platformGap = 600/ platformCount
-      let newPlatformBottom = 100 + i*platformGap
+      let newPlatBottom = 100 + i*platformGap
+      let newPlatform = new Platform(newPlatBottom)
+      
     }
   }
 
