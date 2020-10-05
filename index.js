01d8142bbe4ea6a6e3f5796233381bf386f17dfb
vistritpandey <vistritpandey@gmail.com>
2020-10-05 18:12:09 +0530

    f

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const grid = document.querySelector('.grid')
+  const doodler = document.createElement('div')
+  let doodlerLeftSpace = 50
+  let doodlerBottomSpace =150
+
+  function createDoodler(){
+    grid.appendChild(doodler)
+    doodler.classList.add('doodler')
+    doodler.style.left = doodlerLeftSpace + 'px'
+    doodler.style.bottom = doodlerBottomSpace + 'px'
+  }
+  createDoodler()
+})
