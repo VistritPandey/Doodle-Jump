365807fa22a25319dd1de8b51bfdabd491cdb60a
vistritpandey <vistritpandey@gmail.com>
2020-10-05 18:18:26 +0530

    f

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', () =>{
   const grid = document.querySelector('.grid')
   const doodler = document.createElement('div')
   let doodlerLeftSpace = 50
-  let doodlerBottomSpace =150
+  let doodlerBottomSpace = 150
+  let isGamerOver = false
+  let platformCount = 5
 
   function createDoodler(){
     grid.appendChild(doodler)
@@ -10,5 +12,18 @@ document.addEventListener('DOMContentLoaded', () =>{
     doodler.style.left = doodlerLeftSpace + 'px'
     doodler.style.bottom = doodlerBottomSpace + 'px'
   }
-  createDoodler()
+  function createPlatforms(){
+    for (let i=0;i<platformCount;i++){
+      let platformGap = 600/ platformCount
+      let newPlatformBottom = 100 + i*platformGap
+    }
+  }
+
+  function start(){
+    if (isGamerOver == false){
+      createDoodler()
+      createPlatforms()
+    }
+  }
+  start()
 })
